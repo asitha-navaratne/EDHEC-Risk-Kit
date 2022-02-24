eee1eefb7d4c3e6f926eb1096eda649f7d2526c6
Asitha Navaratne <asithan476@gmail.com>
2022-02-24 23:37:13 +0530

    Added functions for calculating the total market index for the Industry Portfolio returns

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -52,6 +52,37 @@ def get_ind_returns():
     ind.columns = ind.columns.str.strip()
     return ind
 
+def get_ind_size():
+    """
+    Load and format the Ken French 30 Industry Portfolios Average size (market cap)
+    """
+    ind = pd.read_csv("Downloads/data/ind30_m_size.csv", header=0, index_col=0)
+    ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period("M")
+    ind.columns = ind.columns.str.strip()
+    return ind
+
+def get_ind_nfirms():
+    """
+    Load and format the Ken French 30 Industry Portfolios Average number of Firms
+    """
+    ind = pd.read_csv("Downloads/data/ind30_m_nfirms.csv", header=0, index_col=0)
+    ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period("M")
+    ind.columns = ind.columns.str.strip()
+    return ind
+
+def get_total_market_index_returns():
+    """
+    Load the 30 industry portfolio data and derive the returns of a capweighted total market index
+    """
+    ind_nfirms = get_ind_nfirms()
+    ind_size = get_ind_size()
+    ind_return = get_ind_returns()
+    ind_mktcap = ind_nfirms * ind_size
+    total_mktcap = ind_mktcap.sum(axis=1)
+    ind_capweight = ind_mktcap.divide(total_mktcap, axis="rows")
+    total_market_return = (ind_capweight * ind_return).sum(axis="columns")
+    return total_market_return
+
 def semideviation(r):
     """
     Returns the semideviation aka negative semideviation of r
