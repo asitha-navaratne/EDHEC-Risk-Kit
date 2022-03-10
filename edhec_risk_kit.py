03e5e3cbab91c53fcbe977b5986efc2b7e35b92e
Asitha Navaratne <asithan476@gmail.com>
2022-03-10 07:33:34 +0530

    Added function to calculate the value of a dollar at the end of an investing period

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import math
 import scipy.stats
+import seaborn as sns
+import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.optimize import minimize
 
@@ -591,3 +593,45 @@ def bt_mix(r1, r2, allocator, **kwargs):
         raise ValueError("Allocator returned weights that don't match r1")
     r_mix = weights*r1 + (1-weights)*r2
     return r_mix
+
+def terminal_values(rets):
+    """
+    Returns the final values of a dollar at the end of the return period for each scenario
+    """
+    return (rets+1).prod()
+
+def terminal_stats(rets, floor = 0.8, cap=np.inf, name="Stats"):
+    """
+    Produce Summary Statistics on the terminal values per invested dollar
+    across a range of N scenarios
+    rets is a T x N DataFrame of returns, where T is the time-step (we assume rets is sorted by time)
+    Returns a 1 column DataFrame of Summary Stats indexed by the stat name 
+    """
+    terminal_wealth = (rets+1).prod()
+    breach = terminal_wealth < floor
+    reach = terminal_wealth >= cap
+    p_breach = breach.mean() if breach.sum() > 0 else np.nan
+    p_reach = reach.mean() if reach.sum() > 0 else np.nan
+    e_short = (floor-terminal_wealth[breach]).mean() if breach.sum() > 0 else np.nan
+    e_surplus = (-cap+terminal_wealth[reach]).mean() if reach.sum() > 0 else np.nan
+    sum_stats = pd.DataFrame.from_dict({
+        "mean": terminal_wealth.mean(),
+        "std" : terminal_wealth.std(),
+        "p_breach": p_breach,
+        "e_short":e_short,
+        "p_reach": p_reach,
+        "e_surplus": e_surplus
+    }, orient="index", columns=[name])
+    return sum_stats
+
+def glidepath_allocator(r1, r2, start_glide=100, end_glide=0):
+    """
+    Simulates a Target-Date-Fund style gradual move from r1 to r2
+    """
+    n_points = r1.shape[0]
+    n_cols = r1.shape[1]
+    path = pd.Series(data=np.linspace(start_glide, end_glide, num=n_points))
+    paths = pd.concat([path]*n_cols, axis=1)
+    paths.index = r1.index
+    paths.columns = r1.columns
+    return paths
